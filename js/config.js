4089270a31ac6dfaf2e0d58e1d760708b8d543a1
olga12kz-DS <165859038+olga12kz-DS@users.noreply.github.com>
2025-07-05 19:55:19 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_XYwBEf6yD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'q2h34d78o8okdloogls1jsgdd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_kXcwcOcd0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6i5hs4ufj9r8sp7qgdpjgu3fd7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
